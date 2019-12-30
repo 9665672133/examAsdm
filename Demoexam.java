77ff250b438e8f70a9185c00490efcfd8b1d3b6e
kunal444 <35965939+kunal444@users.noreply.github.com>
2019-12-30 17:21:20 +0530

    sussex

---
+++ Demoexam.java
@@ -0,0 +1,10 @@
+
+public class Demoexam {
+
+	public static void main(String[] args) {
+		System.out.println("het there");
+		System.out.println("ujhjgh");
+		System.out.println("gfjgsj");
+	}
+
+}
